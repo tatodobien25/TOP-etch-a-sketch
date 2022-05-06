ab54de55b8c1599ef244fa1e63309f06f89b8be0
Sergio Correa Guerrero <s.sergiocorreag@gmail.com>
2022-05-06 03:06:30 -0500

    created original files and javascript functions for div row and div columns

---
+++ script.js
@@ -0,0 +1,26 @@
+const canvasContainerDiv = document.getElementById('canvas-container');
+const webAppContainer = document.getElementById('web-app-container');
+const lastCreatedDiv = document.createElement('div');
+const textInput = document.getElementById('measurement-input');
+const submitBtn = document.querySelector('button');
+
+function createDivsRow(measurement) {
+    for (let index = 0; index < +measurement; index++) {
+        let divToBeAdded = document.createElement('div');
+        divToBeAdded.classList.add('added-to-row');
+        canvasContainerDiv.appendChild(divToBeAdded);
+    }
+}
+
+function createDivsGrid(measurement) {
+    for (let index = 0; index < +measurement; index++) {
+        let rowContainer = document.createElement('div');
+        rowContainer.classList.add('row-container');
+        canvasContainerDiv.appendChild(rowContainer);
+    }
+}
+
+createDivsRow(10);
+createDivsRow(10);
+
+createDivsGrid(20);
