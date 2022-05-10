9535f4755c316ed7ff9f6158adbfb2d61080a16a
Sergio Correa Guerrero <s.sergiocorreag@gmail.com>
2022-05-09 21:28:33 -0500

    Set up a 'hover' effect so that grid divs change color when mouse passes over them

--- script.js
+++ script.js
@@ -1,16 +1,8 @@
+'use strict'
+
 const canvasContainerDiv = document.getElementById('canvas-container');
 const webAppContainer = document.getElementById('web-app-container');
 const lastCreatedDiv = document.createElement('div');
-const textInput = document.getElementById('measurement-input');
-const submitBtn = document.querySelector('button');
-
-function createDivsRow(measurement) {
-    for (let index = 0; index < +measurement; index++) {
-        let divToBeAdded = document.createElement('div');
-        divToBeAdded.classList.add('added-to-row');
-        canvasContainerDiv.appendChild(divToBeAdded);
-    }
-}
 
 function createDivsGrid(measurement) {
     for (let index = 0; index < +measurement; index++) {
@@ -27,3 +19,15 @@ function createDivsGrid(measurement) {
 }
 
 createDivsGrid(16);
+const divsWithinCanvas = Array.from(document.querySelectorAll('div.added-to-row'));
+divsWithinCanvas.forEach((pixelDiv) => {
+    pixelDiv.addEventListener('mouseover', function (e) {
+        // console.log(e);
+        if (e.buttons == 1) {
+            pixelDiv.classList.add('black');
+            console.log(e.fromElement.classList);
+        }
+
+    });
+
+});
