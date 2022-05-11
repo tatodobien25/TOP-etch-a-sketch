f2cff8731acdc194a409e971e1e305fa6ef8b5b5
Sergio Correa Guerrero <s.sergiocorreag@gmail.com>
2022-05-10 22:01:39 -0500

    improve pixel painting behavior by adding and removing eventListener features

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 const canvasContainerDiv = document.getElementById('canvas-container');
 const webAppContainer = document.getElementById('web-app-container');
 const lastCreatedDiv = document.createElement('div');
+let clickStatus = false;
 
 function createDivsGrid(measurement) {
     for (let index = 0; index < +measurement; index++) {
@@ -18,16 +19,56 @@ function createDivsGrid(measurement) {
     }
 }
 
-createDivsGrid(16);
-const divsWithinCanvas = Array.from(document.querySelectorAll('div.added-to-row'));
-divsWithinCanvas.forEach((pixelDiv) => {
-    pixelDiv.addEventListener('mouseover', function (e) {
-        // console.log(e);
-        if (e.buttons == 1) {
-            pixelDiv.classList.add('black');
-            console.log(e.fromElement.classList);
-        }
+function mouseDownOnPixelDiv() {
+    console.log(this);
+
+    clickStatus = true;
+    console.log(clickStatus);
+    this.style.background = 'darkblue';
+}
+
+function mouseUpOnPixelDiv() {
+    clickStatus = false;
+    console.log(clickStatus);
+    this.style.background = 'darkblue';
+
+}
+
+function mouseOverPixelDiv() {
+    if (clickStatus) {
+        this.style.background = 'darkblue';
+    } else return;
+}
+
+function mouseEnterCanvasHandler() {
+    divsWithinCanvas.forEach((pixelDiv) => {
+        pixelDiv.addEventListener('mousedown', mouseDownOnPixelDiv);
 
     });
+    divsWithinCanvas.forEach((pixelDiv) => {
+        pixelDiv.addEventListener('mouseup', mouseUpOnPixelDiv);
+    });
 
-});
+    divsWithinCanvas.forEach((pixelDiv) => {
+        pixelDiv.addEventListener('mouseover', mouseOverPixelDiv);
+    });
+}
+function mouseLeaveCanvasHandler() {
+    clickStatus = false;
+    divsWithinCanvas.forEach((pixelDiv) => {
+        pixelDiv.removeEventListener('mousedown', mouseDownOnPixelDiv);
+
+    });
+    divsWithinCanvas.forEach((pixelDiv) => {
+        pixelDiv.removeEventListener('mouseup', mouseUpOnPixelDiv);
+    });
+
+    divsWithinCanvas.forEach((pixelDiv) => {
+        pixelDiv.removeEventListener('mouseover', mouseOverPixelDiv);
+    });
+}
+createDivsGrid(16);
+const divsWithinCanvas = Array.from(document.querySelectorAll('div.added-to-row'));
+
+
+
